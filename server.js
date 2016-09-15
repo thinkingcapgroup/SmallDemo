6930f701ab2a72189ce200e8ee35308c8277fbdb
Erick Sauri <ejs3863@rit.edu>
2016-09-14 21:07:12 -0400

    Added Google Authentication for test login.

--- server.js
+++ server.js
@@ -1,24 +1,104 @@
 //modules
-var express = require('express');
-var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var fs = require("fs");
+var express = require('express'),
+		app = express(),
+		handlebars = require('express-handlebars').create({defaultLayout:'main'}),
+		fs = require("fs"),
+		port = process.env.PORT || 3000,
+		cookieParser = require('cookie-parser'),
+		session = require('express-session'),
+		authConfig = require('./config/auth'),
+		passport = require('passport'),
+		GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+
+/**
+ * Passport Authentication
+ */
+passport.serializeUser(function(user, done){
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done){
+	done(null, obj);
+});
+
+passport.use(new GoogleStrategy(
+	authConfig.google,
+	function(request, accessToken, refreshToken, profile, done) {
+		return done(null, profile);
+	}
+));
+
+
+/**
+ * Configure Express
+ */
 
 //security app disable
 app.disable('x-powered-by');
 
 //setting up the port and engine
-app.set('port', process.env.PORT || 3000);
+app.set('port', port);
 app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-//pages
-app.get('/', function(req,res){
-	res.render('home');
+app.use(cookieParser());
+app.use(session({
+	secret: 'cookie_secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+// Initialize Passport
+app.use(passport.initialize());
+
+// Restore Passport Session, if any
+app.use(passport.session());
+
+
+/**
+ * Routes
+ */
+app.get('/', function(req,res, next){
+	var model = {};
+	model.title = 'Mars University';
+
+	res.render('home', model);
 });
 
+app.get('/login', function(req,res){
+	res.render('login');
+});
+
+// GET /auth/google
+// Passport will authenticate using Google
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'] }));
+
+// Get /auth/google/callback
+// If authentication fails, we redirect back to login
+// Otherwise if authentication is successful we redirect to the home page
+app.get('/auth/google/callback',
+  passport.authenticate('google',
+	{ successRedirect: '/',
+	 failureRedirect: '/login'
+ 	}
+));
+/*app.get('auth/google/callback', passport.authenticate('google', {successRedirect: '/', failureRedirect: '/login'}));
+*/
+app.get('/profile', ensureAuthenticated, function(req, res){
+	var model = {};
+	model.user = req.user;
+
+	res.render('profile', {user: req.user});
+});
+
+app.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
+});
 
 //port starting and listening
 app.listen(app.get('port'), function(){
@@ -44,3 +124,10 @@ app.use(function(err, req,res, next){
 	res.status(500);
 	res.render('500');
 });
+
+function ensureAuthenticated(req, res, next){
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+}
